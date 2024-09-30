81e7186b259f6ad1fac99809e447d37115af185a
Jefferson Rizzetto <Jefferson Rizzetto>
2024-09-30 12:00:19 -0300

    Projeto 6 - Todo List

---
+++ Projeto 6 - Todo List - Initial/scripts.js
@@ -0,0 +1,34 @@
+
+const todosApp = {
+    data(){
+        return{
+            todos: [],
+            newTodo: {
+                done: false
+            }
+        }
+    },
+    methods: {
+        addTodo: function(){
+            if(this.newTodo.text){
+                this.todos.push(this.newTodo);
+                this.newTodo = {
+                    done: false
+            };
+            window.localStorage.setItem("todos", JSON.stringify(this.todos));
+            }else{
+                alert("To-do text is required")
+            }
+        }
+    },
+    created() {
+        this.todos = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : this.todos;
+    },
+    updated(){
+        localStorage.setItem("todos", JSON.stringify(this.todos));
+    }
+
+};
+
+Vue.createApp(todosApp).mount('#app');
+
